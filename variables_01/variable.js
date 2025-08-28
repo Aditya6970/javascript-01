575fe7472c9b2851dfe1562fb235ff3cf38c2388
Aditya Rai <142682937+Aditya6970@users.noreply.github.com>
2025-08-28 17:50:01 +0000

    container config commit

---
+++ variables_01/variable.js
@@ -0,0 +1,9 @@
+const AccountId =1233259
+let AccountEmail="aditya@gmail.com"
+AccountPassword ="12345"
+AccountAddress= "Seoni"
+
+
+
+
+console.table([AccountId,AccountEmail,AccountAddress,AccountPassword])
