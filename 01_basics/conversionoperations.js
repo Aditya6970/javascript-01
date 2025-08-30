5b209b8dfdc8a5da2608703dec9881a9274710c6
Aditya Rai <142682937+Aditya6970@users.noreply.github.com>
2025-08-30 18:37:54 +0000

    Conversion operations

--- 01_basics/conversionoperations.js
+++ 01_basics/conversionoperations.js
@@ -14,12 +14,31 @@ let valueInNumber = Number(score)
 let IsLoggedin = 1
 
 let booleanIsLoggedIn = Boolean(IsLoggedin)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1=> TRUE , 0=> FALSE , " " => FALSE , "ADITYA " => TRUE
 
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber)
+
+// **************************Operations*************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%3)
+
+let str1 = "hello"
+let str2 = " aditya"
+let str3 = str1 + str2
+console.log(str3)  // OUTPUT hello aditya
+
