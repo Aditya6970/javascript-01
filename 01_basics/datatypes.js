88208b52b569d8ca74a89397ea1f3040ab000970
Aditya Rai <142682937+Aditya6970@users.noreply.github.com>
2025-08-30 17:54:59 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,27 @@
+"use strict" // treat all JS code as newer version
+// 
+// aleart ( 3+ 3) we are using node.js , not browser
+
+console.log(3+3)
+// code readability should be high  
+console.log("aditya")
+
+
+
+//  tc39.es  and mdn f0r documentation reading
+
+let name = " aditya" // string data type
+
+let age = 19 // number => 2 to the piower 53
+
+let isloggedin = false // boolean
+
+let state = null  // null => standalone value 
+
+// undefined => 
+
+// symbol => unique
+
+// object 
+
+console.log(typeof "aditya")
