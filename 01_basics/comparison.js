f1d0ce486357456b3d23d5318fc4bd48b67148a3
Aditya Rai <142682937+Aditya6970@users.noreply.github.com>
2025-09-02 07:04:15 +0000

    comparison

---
+++ 01_basics/comparison.js
@@ -0,0 +1,22 @@
+/*console.log(2 > 1)
+console.log(2 == 1)
+console.log(2 < 1)
+console.log(2 >= 1)*/
+
+//console.log("2" == 2);
+//console.log("02" == 1);
+
+/*console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);*/ // avoid this to write a clean code as above
+
+
+// ===  strictly check this is used to also check the data type and provides the output according to it
+
+console.log("2" == 1);
+
